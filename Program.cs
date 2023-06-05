77c20833285a028ea243af026e3d195b7ec0f7f0
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-04 23:56:09 -0700

    if statement

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace if_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c = 3;
+            if (a > b || b > c)
+            {
+                Console.WriteLine("condition IF is true");
+
+            }
+            else if(a < b && b == c)
+            {
+                Console.WriteLine("condition ELSE-IF is true");
+
+            }
+
+            else
+            {
+                Console.WriteLine("condition ELSE is true");
+
+            }
+        }
+    }
+}
